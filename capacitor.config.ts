043a225f33d2e9338e24b02d7a43c13022bbff17
Pat211290 <Patwittner25@gmail.com>
2026-09-23 12:24:40 +0200

    Configure Android application shell

---
+++ capacitor.config.ts
@@ -0,0 +1,3 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig={appId:'de.motoratlas.app',appName:'MotorAtlas',webDir:'dist',server:{androidScheme:'https'},android:{allowMixedContent:false}};
+export default config;
