0e51ef6dc9f8a02a96a062bbc04e1eeda5f2aecb
Pat211290 <Patwittner25@gmail.com>
2026-09-23 12:25:35 +0200

    Add MotorAtlas application entrypoint

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './base.css';
+import './marketing.css';
+import './workspace.css';
+
+if ('serviceWorker' in navigator && import.meta.env.PROD) {
+  window.addEventListener('load', () => navigator.serviceWorker.register('/sw.js').catch(() => undefined));
+}
+
+createRoot(document.getElementById('root')!).render(<StrictMode><App/></StrictMode>);
